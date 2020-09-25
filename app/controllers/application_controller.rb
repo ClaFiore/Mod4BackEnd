65c1067a6f86d001164e1dd76d0cc2ec5f22fb4a
ClaFiore <claborghini@gmail.com>
2020-09-25 10:04:54 -0400

    cla_set up controllers and active record associations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    # def encode_token(payload)
+    #     JWT.encode(payload, 'ClaSte', 'HS256')
+    # end
+
+    # def logged_in?
+    #     headers = request.headers["Authorization"]
+    # end
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -1,2 +1,5 @@
 class Table < ApplicationRecord
+    has_many :reservations
+    has_many :users, through: :reservations
+    belongs_to :restaurant
 end
