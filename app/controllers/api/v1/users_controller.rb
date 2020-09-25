0eaa530707df6b9728735e1dce60f23d9e6f9bb0
ClaFiore <claborghini@gmail.com>
2020-09-25 12:13:47 -0400

    cla-users show page

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::UsersController < ApplicationController
         users = User.all 
         render json: users, except: [:updated_at, :created_at], include: [:reservations => {except: [:created_at, :updated_at]}]
     end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user, except: [:updated_at, :created_at], include: [:reservations => {except: [:created_at, :updated_at]}]
+    end
 end
