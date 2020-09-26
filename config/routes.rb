629120192a2fed8319370b8db3eafeadd0368089
slee713 <steven.lee1393@gmail.com>
2020-09-26 11:26:10 -0400

    change routes to have query params

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tables
-      get '/restaurants/:start', to: 'restaurants#index'
       resources :restaurants
       resources :reservations
       resources :users
