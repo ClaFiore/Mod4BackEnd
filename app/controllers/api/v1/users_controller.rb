3cafa55c8cead4b59b4d5898c6b604dbce0ba3ed
ClaFiore <claborghini@gmail.com>
2020-09-26 14:51:24 -0400

    created create method in users controller to sign up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :logged_in?, only: [:create]
+
     def index
         users = User.all 
         render json: users, except: [:updated_at, :created_at], include: [:reservations => {except: [:created_at, :updated_at]}]
@@ -7,5 +9,21 @@ class Api::V1::UsersController < ApplicationController
     def show
         user = User.find_by(id: params[:id])
         render json: user, except: [:updated_at, :created_at], include: [:reservations => {except: [:created_at, :updated_at]}]
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.valid?
+            user.save
+            render json: {username: user.username, token: encode_token({user_id: user.id})}, status: 200
+        else
+            render json: {error: 'Failed to create new account'}, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :first_name, :last_name, :email, :address)
     end
 end
