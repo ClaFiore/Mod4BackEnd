75b8fc57278bdcbbeaabbb79324e595decfd5909
slee713 <steven.lee1393@gmail.com>
2020-09-29 10:16:00 -0400

    fetch restaurant details

--- app/controllers/api/v1/tables_controller.rb
+++ app/controllers/api/v1/tables_controller.rb
@@ -1,2 +1,24 @@
+require 'rest-client'
+
 class Api::V1::TablesController < ApplicationController
+    skip_before_action :logged_in?, only: [:show]
+    def show
+        table = Table.find_by(id: params[:id])
+        zomato_rest_id = table.restaurant.zomato_rest_id
+
+        url = "https://developers.zomato.com/api/v2.1/restaurant?res_id=#{zomato_rest_id}"
+
+        response = Excon.get(
+            url,
+            headers: {
+            'X-RapidAPI-Host' => URI.parse(url).host,
+            'user-key' => "1b316afe0f10805aa36886e94f4f65fe"#"1f5bbdd1226212f97f0e19baadadc96f"
+            }
+        )
+
+        data = JSON.parse(response.body)
+        render json: data
+
+
+    end
 end
